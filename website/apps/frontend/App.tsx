e9b3679545427f5a4eb3078136f35a2accce78f7
julian3sp <julianrespinal24@gmail.com>
2026-03-17 15:37:13 +0100

    she works

--- website/apps/frontend/App.tsx
+++ website/apps/frontend/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-
 import './App.css'
-import HomePage from './src/pages/homepage/HomePage'
+import { HomePage } from './src/pages/homepage/HomePage'
 
 function App() {
   return (
--- website/apps/frontend/src/components/ui/button.tsx
+++ website/apps/frontend/src/components/ui/button.tsx
@@ -51,8 +51,8 @@ function Button({
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
   }) {
-  const Comp = asChild ? Slot.Root : "button"
-
+  const Comp = asChild ? Slot : "button"
+  console.log({ cn });
   return (
     <Comp
       data-slot="button"
