be4db5bd98591a37a5ff8cb1b806c563894ffef2
julian3sp <julianrespinal24@gmail.com>
2026-03-24 17:39:49 +0100

    fixing

--- SOS-Lang/website/apps/backend/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.api import notes, ai, auth
-
-app = FastAPI()
-
-app.include_router(auth.router, prefix="/register")
-app.include_router(auth.router, prefix="/login")
-app.include_router(notes.router, prefix="/newnote")
-app.include_router(ai.router, prefix="/ai")
