4a7b556480deab83ad503b42e1706f0b1b518772
Eturas314 <144499966+Eturas314@users.noreply.github.com>
2026-04-22 12:25:02 +0200

    Added authentication requirement for studymaterial page

--- website/apps/frontend/App.tsx
+++ website/apps/frontend/App.tsx
@@ -11,16 +11,18 @@ import { ProtectedRoute } from './ProtectedRoute'
 import { UserDashboard } from './src/pages/UserDashboard'
 
 function App() {
-  //const [role, setRole] = useState<"guest" | "user">("guest");
 
   return (
     <AuthProvider>
       <div className="min-h-screen w-screen bg-[var(--page-bg)] text-white">
         <NavBar />
         <Routes>
+          {/* Public routes are accessible to all users, regardless of their authentication status */}
           <Route path="/" element={<HomePage />} />
 
           <Route path="/about" element={<About />} />
+
+          {/* Protected routes are used to ensure that only authenticated users can access the pages */}
           <Route path="/notes"
             element={
               <ProtectedRoute>
@@ -28,10 +30,15 @@ function App() {
               </ProtectedRoute>
             }
           />
-          <Route path="/studymaterial">
-            <Route index element={<AIStudyMaterial />} />
-            <Route path=":setId" element={<AIStudyMaterial />} />
-          </Route>
+
+          <Route path="/studymaterial"
+             element={
+              <ProtectedRoute>
+                <AIStudyMaterial />
+              </ProtectedRoute>
+            }
+          />
+          
           <Route path="/userdashboard"
             element={
               <ProtectedRoute>
