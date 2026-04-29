87b086f6ad41f852e2f55f4060a7186d964d748b
julian3sp <julianrespinal24@gmail.com>
2026-04-29 14:51:38 +0200

    stats fix

--- website/apps/backend/app/db/client.py
+++ website/apps/backend/app/db/client.py
@@ -1,4 +1,4 @@
-from supabase import create_client, auth, autoRefreshToken, persistSession, detectSessionInUrl
+from supabase import create_client
 import os
 from dotenv import load_dotenv
 
@@ -7,10 +7,4 @@ load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
 
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY, {
-  auth: {
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: true,
-  }
-});
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
